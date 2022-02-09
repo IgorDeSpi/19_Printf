2d92fb6eefedc0b42e7932702aea14037161a6db
Igor De spirlet <ide-spir@mi-c2-p1.s19.be>
2022-02-09 17:07:45 +0100

    Restarting the project from zero

--- ft_printf.c
+++ ft_printf.c
@@ -5,59 +5,9 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ide-spir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/13 10:57:58 by ide-spir          #+#    #+#             */
-/*   Updated: 2022/01/27 13:47:49 by ide-spir         ###   ########.fr       */
+/*   Created: 2022/02/09 14:04:37 by ide-spir          #+#    #+#             */
+/*   Updated: 2022/02/09 14:06:26 by ide-spir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/ft_printf.h"
-
-t_flags	ft_init_flags(void)
-{
-	t_flags	flags;
-
-	flags.dot = -1;
-	flags.minus = 0;
-	flags.star = 0;
-	flags.type = 0;
-	flags.width = 0;
-	flags.zero = 0;
-	return (flags);
-}
-
-int	ft_flag_parse(const char *cpy, int i, t_flags *flags, va_list args)
-{
-	while (cpy[i])
-	{
-		if (!ft_isdigit(cpy[i]) && !ft_is_type_list(cpy[i]) && !ft_is_flag_list(cpy[i]))
-			break ;
-		if (cpy[i] == '0' && flags->width == 0 && flags->minus == 0)
-			flags->zero = 1;
-		if (cpy[i] == '.')
-			i = ft_flag_dot(cpy, i, flags, args);
-		if (cpy[i] == '-')
-			*flags = ft_flag_minus(*flags);
-		if (cpy[i] == '*')
-			*flags = ft_flag_width(args, *flags);
-		if (ft_isdigit(cpy[i]))
-			*flags = ft_flag_digit(cpy[i], *flags);
-		if (ft_is_type_list(cpy[i]))
-		{
-			flags->type = cpy[i];
-			break ;
-		}
-		i++;
-	}
-	return (i);
-}
-
-int	ft_treat_cpy(const char *cpy, va_list args)
-{
-	int		i;
-	t_flags	flags;
-	int		count_char;
-
-	i = 0;
-	count_char = 0;
-	while
-}
+#include "./includes/ft_printf.h"
--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -5,50 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ide-spir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/13 11:03:22 by ide-spir          #+#    #+#             */
-/*   Updated: 2022/01/27 13:35:27 by ide-spir         ###   ########.fr       */
+/*   Created: 2022/02/09 14:06:05 by ide-spir          #+#    #+#             */
+/*   Updated: 2022/02/09 14:08:28 by ide-spir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "./libft/libft.h"
 # include <stdarg.h>
-
-typedef struct s_flags
-{
-	int				already_print;
-	int				type;
-	int				width;
-	int				minus;
-	int				zero;
-	int				dot;
-	int				star;
-}					t_flags;
-
-int		ft_printf(const char *str, ...);
-int		ft_putchar(int c);
-int		ft_putstr_perc(char *str, int prec);
-int		ft_treat_char(char c, t_flags flags);
-int		ft_flag_dot(const char *save, int start, t_flags *flags, va_list args);
-int		ft_treat_int(int i, t_flags flags);
-int		ft_treat_percent(t_flags flags);
-int		ft_treat_string(char *str, t_flags flags);
-int		ft_treat_width(int width, int minus, int has_zero);
-int		ft_treatment(int c, t_flags flags, va_list args);
-int		ft_treat_hexa(unsigned int ui, int lower, t_flags flags);
-int		ft_treat_ptr(unsigned long long ull, t_flags flags);
-int		ft_is_flag_list(int c);
-int		ft_is_type_list(int c);
-int		ft_treat_uint(unsigned int ui, t_flags flags);
-
-char	*ft_str_tolower(char *str);
-char	*ft_ull_base(unsigned long long ull, int base);
-char	*ft_u_itoa(unsigned int n);
-
-t_flags	ft_flag_minus(t_flags flags);
-t_flags	ft_flag_digit(char c, t_flags flags);
-t_flags	ft_flag_width(va_list args, t_flags flags);
+# include <unistd.h>
 
 #endif
